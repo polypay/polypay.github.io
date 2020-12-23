e7057e14cbd50729689373605d18ac6bb222640e
Pat Schloss <pschloss@umich.edu>
2020-12-23 16:10:35 -0500

    Update membership through December 2020

--- _rcode/compare_registration.R
+++ _rcode/compare_registration.R
@@ -53,7 +53,7 @@ geocodeAdddress <- function(address) {
 }
 
 
-directory <- "assets/xlsx/directory-2020-08-01.xlsx"
+directory <- "assets/xlsx/directory-2021-01-01.xlsx"
 
 exclude_columns <- c("Active/Inactive", "Dues Year Paid", "First Names", "Last Name", "Fax Number")
 
